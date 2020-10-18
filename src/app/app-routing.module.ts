b140e31152a298e87f806c659fd82960d96e10a5
adigibli <gibliadi@gmail.com>
2020-10-18 13:51:12 +0300

    New Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { BookListComponent } from './books/book-list/book-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/books', pathMatch: 'full'},
+  {path: 'books', component: BookListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookItemComponent } from './books/book-list/book-item/book-item.component';
+import { BookEditComponent } from './books/book-edit/book-edit.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookItemComponent,
+    BookEditComponent,
+    BookDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
